9f02444e2d68075df2cb39d8df69571d93dc6265
Random Person <random_person@something.com>
2022-05-27 19:50:13 +0000

    fix

---
+++ devices/poco_m3/kernel/kernel_download.sh
@@ -0,0 +1,11 @@
+set -xe
+rm -rf k
+#git clone https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b lime-q-oss k
+#wget -O k.zip https://github.com/MiCode/Xiaomi_Kernel_OpenSource/archive/refs/heads/lime-q-oss.zip
+#wget -O k.zip https://github.com/MiCode/lattice0/archive/refs/heads/lime-q-oss.zip
+#git clone https://github.com/lattice0/Xiaomi_Kernel_OpenSource -b lime-q-oss k
+#git clone https://github.com/gaganmalvi/kernel_xiaomi_lime -b Q k
+#git clone https://github.com/c3eru/Q k && cd k && git checkout 355bd900336d6c909e5b8743c50e9669119c0b8d
+#git clone https://github.com/c3eru/kernela10 k
+#git clone https://github.com/c3eru/a10tes k
+git clone https://github.com/MoonBase-Project/MSM-4.19-MiBengal k

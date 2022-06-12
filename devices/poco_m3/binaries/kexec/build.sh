114844ef88d5b9b6cde01188b67e887d6b0ff367
Lattice 0 <lattice0@protonmail.com>
2022-06-12 01:03:25 +0000

    log

--- devices/poco_m3/binaries/kexec/build.sh
+++ devices/poco_m3/binaries/kexec/build.sh
@@ -11,7 +11,7 @@ export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
 export LD=$TOOLCHAIN/bin/ld
 export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
 export STRIP=$TOOLCHAIN/bin/llvm-strip
-(cd kex && autoreconf && LDFLAGS=-static ./configure --host $TARGET && make)
+(cd kex && autoreconf && LDFLAGS=-static ./configure --host $TARGET || cat config.log && make)
 # needed because of https://github.com/termux/termux-packages/issues/8273
 ../align_fix.py ${SCRIPT_DIR}/kex/build/sbin/kexec
 #--sysroot=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/sysroot/ 
